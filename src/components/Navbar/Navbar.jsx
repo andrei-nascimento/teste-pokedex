ceb788a4d1a00f6bfd8e2621385512e3034d4e21
Andrei Nascimento <andrei.lima.nas@outlook.com>
2023-02-09 14:11:53 -0300

    Criação do layout e integração com a API

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,73 @@
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+import Avatar from '@mui/material/Avatar';
+import PokebolaIcon from '../../assets/pokebola.png'; 
+import "./Navbar.css";
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(0),
+        width: 'auto',
+    },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+        width: '12ch',
+        '&:focus': {
+            width: '20ch',
+        },
+        },
+    },
+}));
+
+export default function Navbar() {
+    return (
+        <Box sx={{ flexGrow: 1, marginBottom: 4 }}>
+            <AppBar position="static" sx={{ backgroundColor: '#003a70' }}>
+                <Toolbar className='navbar-space'>
+                    <Search>
+                        <SearchIconWrapper>
+                            <SearchIcon />
+                        </SearchIconWrapper>
+                        <StyledInputBase
+                            placeholder="Buscar..."
+                            inputProps={{ 'aria-label': 'search' }}
+                        />
+                    </Search>
+                    <Avatar alt="Pokebola" src={PokebolaIcon} />
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Grid, Container } from "@mui/material";
+import PokeCard from "../../components/PokeCard/PokeCard";
+import Navbar from "../../components/Navbar/Navbar";
+import "./Home.css";
+
+export const Home = () => {
+
+    const [pokemons, setPokemons] = useState([]);
+
+    useEffect(() => {
+        getPokemons();
+    }, []);
+
+    const getPokemons = () => {
+        let endpoints = []
+        for(let i = 1; i <= 20; i++) {
+            endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}`);
+        }
+
+        let response = axios.all(endpoints.map((endpoint) => axios.get(endpoint)))
+            .then((res) => setPokemons(res));
+
+        // axios.get("https://pokeapi.co/api/v2/pokemon?limit=20")
+        // .then((res) => setPokemons(res.data.results))
+        // .catch((err) => console.log(err))
+    };
+
+    return(
+        <>
+            <Navbar />
+            <Container maxWidth="false">
+                <Grid container spacing={4}>
+                    {pokemons.map((pokemon, key) => (
+                        <Grid item xs={2} key={key}>
+                            <PokeCard 
+                                name={pokemon.data.name}
+                                image={pokemon.data.sprites.front_default}    
+                            />
+                        </Grid>
+                    ))}
+                </Grid>
+            </Container>
+        </>
+    );
+}
